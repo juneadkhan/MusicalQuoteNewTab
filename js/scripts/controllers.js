5d115ca7ef17f3bf872e6698733ea1940db306a2
Nuno Goncalves <goncalvesnuno01@gmail.com>
2020-09-30 17:54:07 +0100

    Test to find the longitude and latitude of the user

--- js/scripts/controllers.js
+++ js/scripts/controllers.js
@@ -1,3 +1,18 @@
+var x = document.getElementById("demo");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
 define(['angularAMD', 'storage', 'moment-src'], function (angularAMD, storage, moment) {
 	var controllers = angular.module('controllers', ['services']);
 	controllers.controller('DashboardCtrl', function ($scope, $interval, $http, LS, locale) {
